0bcf42697ad2953041cb9b47a6d334d72e9cad6e
Felipe Bazzarella <fbazzarella@gmail.com>
2023-06-30 16:58:41 -0300

    Adding some new aliases

--- provision.sh
+++ provision.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get -y upgrade
-
-sudo apt-get install -y curl git gnupg2
+sudo apt update -qq
+sudo apt upgrade -y
+sudo apt install -y curl git gnupg2 libpq-dev postgresql-contrib
 
 mkdir -p ~/.config/colorls
 cp -f config/dark_colors.yaml ~/.config/colorls/dark_colors.yaml
-
-cp -f config/bash_aliases ~/.bash_aliases
+ln -sf ~/projects/_environment/config/bash_aliases ~/.bash_aliases
 cp -f config/bashrc ~/.bashrc
 cp -f config/gitconfig ~/.gitconfig
-cp -f config/pryrc ~/.pryrc
 
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io | bash -s stable --auto-dotfiles
 
 source "$HOME/.rvm/scripts/rvm"
 
-rvm install 3.1.3
-rvm alias create default 3.1.3@global
-rvm use 3.1.3@global
+rvm install 3.2.0
+rvm alias create default 3.2.0@global
+rvm use 3.2.0@global
 rvm rvmrc warning ignore allGemfiles
 rvm cleanup all
 
