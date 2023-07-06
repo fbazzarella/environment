9524f31aacd13d44837f02f2b1e0810520953166
Felipe Bazzarella <fbazzarella@gmail.com>
2023-07-06 15:05:45 -0300

    Reorganizing directories and adding some new aliases

--- provision.sh
+++ provision.sh
@@ -2,30 +2,27 @@
 
 sudo apt update -qq
 sudo apt upgrade -y
-sudo apt install -y curl git gnupg2 libpq-dev postgresql-contrib
-
-mkdir -p ~/.config/colorls
-cp -f config/dark_colors.yaml ~/.config/colorls/dark_colors.yaml
-ln -sf ~/projects/_environment/config/bash_aliases ~/.bash_aliases
-cp -f config/bashrc ~/.bashrc
-cp -f config/gitconfig ~/.gitconfig
+sudo apt install -y curl git gnupg2
 
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+
 curl -sSL https://get.rvm.io | bash -s stable --auto-dotfiles
 
 source "$HOME/.rvm/scripts/rvm"
 
-rvm install 3.2.0
-rvm alias create default 3.2.0@global
-rvm use 3.2.0@global
+RUBY_VERSION="3.2.2"
+
+rvm install $RUBY_VERSION
+rvm alias create default $RUBY_VERSION@global
+rvm use $RUBY_VERSION@global
 rvm rvmrc warning ignore allGemfiles
 rvm cleanup all
 
 gem install colorls
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.2/install.sh | bash
+mkdir -p ~/.config/colorls
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-nvm install --lts --default
+ln -sf ~/projects/_environment/configs/dark_colors.yaml ~/.config/colorls/dark_colors.yaml
+ln -sf ~/projects/_environment/dotfiles/bash_aliases ~/.bash_aliases
+ln -sf ~/projects/_environment/dotfiles/bashrc ~/.bashrc
+ln -sf ~/projects/_environment/dotfiles/gitconfig ~/.gitconfig
