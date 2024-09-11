080aaea15dc6eaac36023f713fc57ddc3f06b213
Felipe Bazzarella <fbazzarella@gmail.com>
2024-09-11 12:26:04 -0300

    Updating ruby version and some aliases

--- provision.sh
+++ provision.sh
@@ -10,16 +10,16 @@ curl -sSL https://get.rvm.io | bash -s stable --auto-dotfiles
 
 source "$HOME/.rvm/scripts/rvm"
 
-RUBY_VERSION="3.1.4"
+RUBY_VERSION="3.3.4"
 
-rvm install $RUBY_VERSION
+rvm install ruby-$RUBY_VERSION --with-gcc=gcc-11
 rvm alias create default $RUBY_VERSION@global
 rvm use $RUBY_VERSION@global
 rvm rvmrc warning ignore allGemfiles
 rvm cleanup all
 
 gem update --system
-gem install colorls rails srb debug htmlbeautifier
+gem install colorls
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.2/install.sh | bash
 
