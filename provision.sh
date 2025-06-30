b3a622bf439dc39351287e1aad0f79400c76d5eb
Felipe Bazzarella <fbazzarella@gmail.com>
2025-06-30 18:20:39 -0300

    Updating provisioning and some aliases

--- provision.sh
+++ provision.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
 
-sudo apt update -qq
-sudo apt upgrade -y
-sudo apt install -y curl git gnupg2 libpq-dev postgresql-contrib
+sudo apt install -y curl gnupg2 libpq-dev
 
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-
-curl -sSL https://get.rvm.io | bash -s stable --auto-dotfiles
-
+curl -sSL https://get.rvm.io | bash -s stable -- --ignore-dotfiles
 source "$HOME/.rvm/scripts/rvm"
 
-RUBY_VERSION="3.3.4"
+RUBY_VERSION="3.4.4"
 
-rvm install ruby-$RUBY_VERSION --with-gcc=gcc-11
+rvm install ruby-$RUBY_VERSION
 rvm alias create default $RUBY_VERSION@global
 rvm use $RUBY_VERSION@global
 rvm rvmrc warning ignore allGemfiles
 rvm cleanup all
 
 gem update --system
-gem install colorls
+gem install colorls rails
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.40.3/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 nvm install --lts --default
-npm install --global yarn
+npm install --global npm@11.4.2 yarn vite
 yarn config set "strict-ssl" false
 
 mkdir -p ~/.config/colorls
